92554fc103122f61f6677a2f6154f5406da84fcf
Simran Vedpathak <80968391+SimranSv@users.noreply.github.com>
2022-12-05 12:25:06 +0530

    Add files via upload

---
+++ middle_of_linkedlist.cpp
@@ -0,0 +1,68 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define pb push_back
+#define vll vector<ll>
+#define stll set<ll>
+#define msgll multiset<ll, greater<>>
+#define msll multiset<ll>
+#define pll pair<ll, ll>
+#define mpll map<ll, ll>
+#define vpll vector<pair<ll, ll>>
+#define ss second
+#define ff first
+#define read(n, x)             \
+    for (ll i = 0; i < n; i++) \
+    {                          \
+        ll k;                  \
+        cin >> k;              \
+        x.pb(k);               \
+    }
+#define rep(i, j, k) for (ll i = j; i < k; i++)
+#define st(v) sort(v.begin(), v.end())
+#define sz(x) (ll) x.size()
+#define no cout << "No\n";
+#define yes cout << "Yes\n";
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* node=head;
+        ll cnt=0;
+        while(node!=NULL){
+            
+            cnt++;
+            node=node->next;
+        }
+        ll kmid=cnt/2;
+        ListNode *temp=head;
+        if(cnt%2){
+            // kmid ++;
+            
+            while(kmid){
+                temp=temp->next;
+                kmid--;
+                
+            }
+            
+        }else {
+             while(kmid){
+                temp=temp->next;
+                 kmid--;
+                
+            }
+            
+        }
+        return temp;
+        
+    }
+};
